38eb79adfa966514a319df40cb6048df8c406fea
Evann Gehin <evann.gehin@ezcorp.io>
2020-11-27 20:21:39 +0100

    First commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const options = {};
+    let slider = new Cachu(document.querySelector('.cachu__container'), options);
+    slider.run();
+});
